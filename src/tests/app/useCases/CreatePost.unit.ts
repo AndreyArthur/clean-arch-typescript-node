8503b1f48ee5b8bede1f5b5a0429807f0075c34d
AndreyArthur <twekylaywer@gmail.com>
2021-09-15 16:53:03 -0300

    test: cover CreatePostUseCase

---
+++ src/tests/app/useCases/CreatePost.unit.ts
@@ -0,0 +1,78 @@
+import { CreatePostUseCase } from '@/application/useCases';
+import { PostsRepository } from '@/application/repositories';
+import { PostsRepositoryMemory } from '@/infra/repositories';
+import { uuid } from '@/infra/helpers';
+import { generators, verifiers } from '@/tests/helpers';
+import { MissingFieldsError } from '@/application/exceptions';
+
+type SetupComponents = {
+  postsRepository: PostsRepository;
+  createPost: CreatePostUseCase;
+};
+
+const setup = (): SetupComponents => {
+  const postsRepository = new PostsRepositoryMemory();
+  const createPost = new CreatePostUseCase({
+    repositories: {
+      posts: postsRepository,
+    },
+  });
+
+  return {
+    createPost,
+    postsRepository,
+  };
+};
+
+describe('CreatePost UseCase', () => {
+  it('should create a Post successfully', async () => {
+    const { createPost } = setup();
+    const post = await createPost.execute({
+      title: generators.title(),
+      content: generators.content(),
+      userId: uuid.v4(),
+    });
+
+    expect(verifiers.isPost(post)).toBe(true);
+  });
+
+  it('should fail because fields are missing', async () => {
+    const { createPost } = setup();
+
+    try {
+      await createPost.execute({
+        title: '',
+        content: generators.content(),
+        userId: uuid.v4(),
+      });
+
+      throw null;
+    } catch (err) {
+      expect(err instanceof MissingFieldsError).toBe(true);
+    }
+
+    try {
+      await createPost.execute({
+        title: '',
+        content: '',
+        userId: uuid.v4(),
+      });
+
+      throw null;
+    } catch (err) {
+      expect(err instanceof MissingFieldsError).toBe(true);
+    }
+
+    try {
+      await createPost.execute({
+        title: generators.title(),
+        content: '',
+        userId: uuid.v4(),
+      });
+
+      throw null;
+    } catch (err) {
+      expect(err instanceof MissingFieldsError).toBe(true);
+    }
+  });
+});
