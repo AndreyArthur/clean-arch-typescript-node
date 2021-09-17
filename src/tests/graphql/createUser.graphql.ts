d4130bfd942b254bb6bed3460e7f6986dfb9d0f9
AndreyArthur <twekylaywer@gmail.com>
2021-09-17 18:39:01 -0300

    refactor: move graphql http request to a helper method

--- src/tests/graphql/createUser.graphql.ts
+++ src/tests/graphql/createUser.graphql.ts
@@ -4,87 +4,74 @@ import { app } from '@/main/app';
 import { generators, verifiers } from '@/tests/helpers';
 import { data as dataSource } from '@/infra/sources';
 
+type Fetch = (args: { username: string, password: string }) => Promise<{
+  data?: any;
+  errors?: any;
+}>;
+
+const fetch: Fetch = async ({ username, password }) => {
+  const { body } = await request(app).post('/graphql/')
+    .send({
+      query: `mutation {
+          user: createUser(
+            username: "${username}",
+            password: "${password}"
+          ) {
+            id
+            username
+            createdAt
+            updatedAt
+          }
+        }`,
+    });
+
+  if (body.data) {
+    body.data.user = {
+      ...body.data.user,
+      createdAt: new Date(body.data.user.createdAt),
+      updatedAt: new Date(body.data.user.updatedAt),
+    };
+  }
+
+  return body;
+};
+
 describe('createUser Mutation', () => {
   afterEach(() => { dataSource.users = []; });
 
   it('should create an User successfully', async () => {
-    const { body: { data } } = await request(app).post('/graphql/').send({
-      query: `mutation {
-        user: createUser(
-          username: "${generators.username()}",
-          password: "${generators.password()}"
-        ) {
-          id
-          username
-          createdAt
-          updatedAt
-        }
-      }`,
+    const { data } = await fetch({
+      username: generators.username(),
+      password: generators.password(),
     });
-    const user = {
-      id: data.user.id,
-      username: data.user.username,
-      createdAt: new Date(data.user.createdAt),
-      updatedAt: new Date(data.user.updatedAt),
-    };
 
-    expect(verifiers.isUser(user, false)).toBe(true);
+    expect(verifiers.isUser(data.user, false)).toBe(true);
   });
 
   it('should fail because fields are missing', async () => {
     {
-      const { body: { errors: [error] } } = await request(app).post('/graphql/')
-        .send({
-          query: `mutation {
-          createUser (
-            username: "",
-            password: ""
-          ) {
-            id
-            username
-            createdAt
-            updatedAt
-          }
-        }`,
-        });
+      const { errors: [error] } = await fetch({
+        username: '',
+        password: '',
+      });
 
       expect(error.name).toBe('MissingFieldsError');
     }
 
     {
-      const { body: { errors: [error] } } = await request(app).post('/graphql/')
-        .send({
-          query: `mutation {
-          createUser (
-            username: "${generators.username()}",
-            password: ""
-          ) {
-            id
-            username
-            createdAt
-            updatedAt
-          }
-        }`,
-        });
+      const { errors: [error] } = await fetch({
+        username: generators.username(),
+        password: '',
+      });
 
       expect(error.name).toBe('MissingFieldsError');
     }
 
     {
-      const { body: { errors: [error] } } = await request(app).post('/graphql/')
-        .send({
-          query: `mutation {
-          createUser (
-            username: "",
-            password: "${generators.password()}"
-          ) {
-            id
-            username
-            createdAt
-            updatedAt
-          }
-        }`,
-        });
+      const { errors: [error] } = await fetch({
+        username: '',
+        password: generators.password(),
+      });
 
       expect(error.name).toBe('MissingFieldsError');
     }
@@ -97,20 +84,10 @@ describe('createUser Mutation', () => {
       username,
     }));
 
-    const { body: { errors: [error] } } = await request(app).post('/graphql/')
-      .send({
-        query: `mutation {
-          createUser (
-            username: "${username}",
-            password: "${generators.password()}"
-          ) {
-            id
-            username
-            createdAt
-            updatedAt
-          }
-        }`,
-      });
+    const { errors: [error] } = await fetch({
+      username,
+      password: generators.password(),
+    });
 
     expect(error.name).toBe('UserExistsError');
   });
